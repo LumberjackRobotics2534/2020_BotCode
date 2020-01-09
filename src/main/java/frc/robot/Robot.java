dbaac476081c1ad0ed689808ef3e4918616e6ca2
lhazard22 <lhazard22@boyslatinmd.com>
2020-01-09 12:20:24 -0500

    Color Sensor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,11 +4,15 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.util.Color;
+import com.revrobotics.ColorSensorV3;
+import com.ctre.phoenix.motorcontrol.can.*;
 
 public class Robot extends TimedRobot {
 
-
+  I2C.Port i2cPort = I2C.Port.kOnboard;
+  ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
   
   @Override
   public void robotInit() {
@@ -16,6 +20,13 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotPeriodic() {
+    Color detectedColor = colorSensor.getColor();
+    double IR = colorSensor.getIR();
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    SmartDashboard.putNumber("IR", IR);
+
   }
 
   @Override
